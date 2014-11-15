9d216942a32b35d63b5d54555466cb926a85f7f1
imarkwick <izzymarkwick@gmail.com>
2014-11-15 21:58:20 +0000

    Going to results page

--- features/step_definitions/starting_game_steps.rb
+++ features/step_definitions/starting_game_steps.rb
@@ -11,7 +11,7 @@ When(/^Press the "(.*?)" button$/) do |button_name|
 end
 
 Then(/^I should be taken to the game page$/) do
-	expect(page).to have_content("Welcome to rock, paper, scissors")
+	expect(page).to have_content("welcome to rock, paper, scissors")
 end
 
 Given(/^I have registered to play$/) do
@@ -25,6 +25,6 @@ When(/^I choose "(.*?)"$/) do |button_name|
 end
 
 Then(/^I should see the "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+	expect(page).to have_content
 end
 
